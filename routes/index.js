f0ccdef114ae53b80ef85029920f987cf5272522
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-05-28 16:34:24 +0700

    Added some more initials and getter for all list of available fac and maj

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const student = require("../controllers/student");
 const router = express.Router();
 
 router.get("/", student.getStudentByQuery);
-router.get("/test", (req, res) => {
-    res.json({"message": "Success!"})
+router.get("/faculties", (req, res) => {
+    res.json(require('../json/list_fakultas.json'))
+})
+router.get("/majors", (req, res) => {
+    res.json(require('../json/list_jurusan.json'))
 })
 
 module.exports = router;
